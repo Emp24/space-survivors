c9d0c81da67cbe8cf3dbc69683a6300d3b4cfd09
Emp <40623900+Emp24@users.noreply.github.com>
2024-04-04 01:56:10 +0200

    Seeker movement

--- Assets/Scripts/Enemy/Seeker.cs
+++ Assets/Scripts/Enemy/Seeker.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Seeker : MonoBehaviour, IEnemy
@@ -11,15 +10,33 @@ public class Seeker : MonoBehaviour, IEnemy
     {
 
         Rotation(player.transform.position);
-        // if (Time.time >= nextMovementTime)
-        // {
-        //     Movement(player.transform.position);
-        //     nextMovementTime = Time.time + _enemyData.movementSpeed;
-        // }
+        if (Time.time >= nextMovementTime)
+        {
+            Movement(player.transform.position);
+            nextMovementTime = Time.time + _enemyData.movementSpeed;
+        }
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        string collidingObjectTag = other.gameObject.tag;
+        string sourceTag = other.gameObject.GetComponent<Projectile>().source.tag;
+
+        if (collidingObjectTag == "Player" && sourceTag == "Player")
+        {
+            TakeDamage(other.gameObject.GetComponent<Projectile>().damage);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _enemyData.health -= damage;
     }
     public void Movement(Vector2 playerPosition)
     {
 
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition, 10 * Time.deltaTime);
     }
     public void Rotation(Vector2 playerPosition)
     {
