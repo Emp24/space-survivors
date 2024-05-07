7cdded057c72edf8c81a4bb1b1c616b9b628d19b
Emp <40623900+Emp24@users.noreply.github.com>
2024-05-07 23:39:51 +0300

    Added Gamemanger and TMP in preparation for adding player powers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System;
+public enum GameState
+{
+    MainMenu,
+    Playing,
+    PowerMenu,
+    GameOver,
+    Pause,
+}
+//Controls the flow of the game
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameState gameState = GameState.MainMenu;
+
+    public event Action<GameState> OnGameStateChanged;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void ChangeGameState(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.MainMenu:
+                gameState = GameState.MainMenu;
+                break;
+            case GameState.Playing:
+                gameState = GameState.Playing;
+                break;
+            case GameState.PowerMenu:
+                gameState = GameState.PowerMenu;
+                break;
+            case GameState.GameOver:
+                gameState = GameState.GameOver;
+                break;
+            case GameState.Pause:
+                gameState = GameState.Pause;
+                break;
+        }
+        OnGameStateChanged?.Invoke(gameState);
+    }
+}
